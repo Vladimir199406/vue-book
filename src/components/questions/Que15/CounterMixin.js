7158f21c322daa96f23823368abcc4eff0aef1f1
vo <vovasso550@rambler.ru>
2021-12-28 02:08:14 +0300

    que-15

---
+++ src/components/questions/Que15/CounterMixin.js
@@ -0,0 +1,12 @@
+export default {
+    data() {
+        return {
+            counter: 0,
+        }
+    },
+    methods: {
+        incrementCounter() {
+            return this.counter += 1;
+        }
+    }
+}
