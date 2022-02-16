31c6e97bccd4ebd743eb56e8aecefeb1c5f2eb22
vo <vovasso550@rambler.ru>
2022-02-17 00:21:22 +0300

    proto: need to add native to all actions + custom examples

---
+++ src/components/questions/Que20-animations/Que20Wrapper.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="que-20" class="block">
+        <h3>
+            Вопрос 20: Анимации во Vue
+        </h3>
+        <p>
+            Описание.
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Que20Wrapper',
+        data(){
+            return {}
+        },
+        components: {}
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
